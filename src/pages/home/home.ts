60a3a93258bd7998943442a8e21b90867863b02f
thytng <tnguye10@macalester.edu>
2018-10-16 11:45:40 -0500

    Basic homepage UI

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,23 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  curInput;
+  stopNumber;
+  stops = [];
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  receiveStopNum() {
+    this.stopNumber = this.curInput;
+    this.stops.push(this.stopNumber);
+  }
+
+  // create a list of cards, loop over the indices
+  closeCard(stop) {
+    var index = this.stops.indexOf(stop, 0);
+    this.stops.splice(index, 1);
   }
 
 }
