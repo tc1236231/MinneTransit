73d0c16acd94824ede91e65bbc92552bd797f3e7
tc1236231 <tc1236231@tc1236231deMacBook-Pro.local>
2018-12-05 12:37:24 -0600

    Fix filtering issues

--- src/pages/filter/filter.ts
+++ src/pages/filter/filter.ts
@@ -7,12 +7,13 @@ import { StopForm } from '../../models/stop-form';
     templateUrl: 'filter.html'
 })
 export class FilterPage {
-    stop: StopForm // The stop whose filter setting is to be changed
+    stop: StopForm; // The stop whose filter setting is to be changed
     selectAll : boolean;
-    unTrackedRouteDirs: RouteDir[] // The list of route-directions to be tracked
-    allRouteDirs: RouteDir[] // The list of route-directions serving this stop
-    selected: boolean[] 
-     constructor(public navCtrl: NavController, private navParams: NavParams) {
+    unTrackedRouteDirs: RouteDir[]; // The list of route-directions to be tracked
+    allRouteDirs: RouteDir[]; // The list of route-directions serving this stop
+    selected: boolean[];
+
+    constructor(public navCtrl: NavController, private navParams: NavParams) {
         this.stop = this.navParams.get("stop");
         //this.stop.refreshRDList();
         this.unTrackedRouteDirs = this.stop.unTrackedRouteDirs;
@@ -22,38 +23,52 @@ export class FilterPage {
         {
             this.selected[i] = true;
         }
-        console.log(this.selected);
         for(let routeDir of this.unTrackedRouteDirs)
         {
             let index = routeDir.indexInArray(this.allRouteDirs);
             if(index != -1)
+            {
                 this.selected[index] = false;
+            }
         }
+        this.updateSelectAll();
+        //console.log(this.selected);
     }
+
      /**
      * Enables or diables individual RD settings.
      */
     updateTracksAllRDs(ev): void {
-        if (ev.value) {
-            this.selectAll = true;
+        if (this.selectAll) {
             for(let i = 0; i < this.selected.length; i++)
             {
                 this.selected[i] = true;
             }
         } else {
-            this.selectAll = false;
             for(let i = 0; i < this.selected.length; i++)
             {
                 this.selected[i] = false;
             }
         }
     }
-    updateCheckbox(routeDir, ev): void {
-        if (!ev.value)
-            this.unTrackedRouteDirs.push(routeDir);
-        else
-            this.unTrackedRouteDirs.splice(this.unTrackedRouteDirs.indexOf(routeDir), 1);
+
+    updateSelectAll()
+    {
+        let isSelectingAll : boolean = true;
+        for(let i = 0; i < this.selected.length; i++)
+        {
+            if(this.selected[i] == false)
+            {
+                isSelectingAll = false;
+            }
+        }
+        this.selectAll = isSelectingAll;        
     }
+
+    updateCheckbox(routeDir, ev): void {
+        this.updateSelectAll();
+    }
+
     submitSettings(): void {
         this.unTrackedRouteDirs = [];
         for(let i = 0; i < this.selected.length; i++)
@@ -61,7 +76,7 @@ export class FilterPage {
             if(!this.selected[i])
                 this.unTrackedRouteDirs.push(this.allRouteDirs[i]);
         }
-        console.log(this.unTrackedRouteDirs);
+        //console.log(this.unTrackedRouteDirs);
         this.stop.unTrackedRouteDirs = this.unTrackedRouteDirs;
         //this.stop.refilter();
         this.navCtrl.pop();
