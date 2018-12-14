92e14f11cc526b69252375060c5267c2532e4872
thytng <tnguye10@macalester.edu>
2018-12-14 08:16:59 -0600

    Fixed doubling issue in favorites page

--- src/pages/favorite/favorite.ts
+++ src/pages/favorite/favorite.ts
@@ -18,53 +18,56 @@ import { StopData } from '../../models/stop-data';
     }
 
     ionViewDidEnter() {
-        console.log("entered favorites page");
         this.loadBookmarkedStops();
 
         let feedbackData = this.navParams.get("stopDatas");
         if (feedbackData !== undefined && Array.isArray(feedbackData)) {
-            for (let i = 0; i < feedbackData.length; i++) {
-                this.bookmarkStop(feedbackData[i].stop_id, feedbackData[i].stop_name);
-            }
+
+            this.bookmarkStop(feedbackData);
+            // for (let data of feedbackData) {
+            //     this.bookmarkStop(data.stop_id, data.stop_name);
+            // }
         }
-        this.events.subscribe('onStopSelectedForBookmark', (prm) => {
-            if (prm.stop_id !== undefined && prm.stop_name !== undefined) {
-                console.log("onStopSelectedForBookmark");
-                this.bookmarkStop(prm.stop_id, prm.stop_name)
-            }
-          });
+
+        // this.events.subscribe('onStopSelectedForBookmark', (prm) => {
+        //     if (prm.stop_id !== undefined && prm.stop_name !== undefined) {
+        //         console.log("onStopSelectedForBookmark");
+        //         this.bookmarkStop(prm.stop_id, prm.stop_name)
+        //     }
+        //   });
     }
 
     loadBookmarkedStops() {
         this.favoritesID = [];
         this.storage.ready().then(() => {
             this.storage.get('Saved stops').then((savedStops) => {
-                if (savedStops !== null && savedStops.length !== 0) {
+                if (savedStops !== null && savedStops != []) {
                     console.log("Bookmarked stops");
                     console.log(savedStops);
                     this.favorites = savedStops;
+                    for (let stop of this.favorites) {
+                        this.favoritesID.push(stop.id);
+                    }
                 } else {
                     console.log("storage empty");
                     this.storage.set('Saved stops', []);
                     this.favorites = [];
                 }
-                for (let stop of this.favorites) {
-                    this.favoritesID.push(stop.id);
-                }
-                console.log(this.favoritesID);
             })
         })
     }
 
-    bookmarkStop(id: number, name: string) {
-        console.log("bookmarking stop");
+    bookmarkStop(dataArray: StopData[]) {
         this.storage.ready().then(() => {
             this.storage.get('Saved stops').then((savedStops) => {
-                console.log(this.favoritesID.indexOf(id));
-                console.log(savedStops.indexOf(id));
-                if (this.favoritesID.indexOf(id) === -1) {
-                    savedStops.push({"id": id, "name": name})
+            for (let data of dataArray) {
+                console.log(savedStops);
+                if (this.favoritesID.indexOf(data.stop_id) == -1) {
+                    console.log("Adding stop to bookmark");
+                    console.log(data);
+                    savedStops.push({"id": data.stop_id, "name": data.stop_name});
                 }
+            };
             this.storage.set('Saved stops', savedStops);
             this.loadBookmarkedStops();
         })
@@ -72,6 +75,26 @@ import { StopData } from '../../models/stop-data';
 }
 
 
+//     bookmarkStop(id: number, name: string) {
+//         console.log("bookmarking stop");
+//         if (this.favoritesID.indexOf(id) === -1) {
+//             this.storage.get('Saved stops').then((savedStops) => {
+//                 savedStops.push({"id": id, "name": name});
+//                 this.storage.set('Saved stops', savedStops);
+//                 this.loadBookmarkedStops();
+//             })
+//         }
+
+//     //     this.storage.ready().then(() => {
+//     //         this.storage.get('Saved stops').then((savedStops) => {
+//     //             if (this.favoritesID.indexOf(id) == -1) {
+//     //                 savedStops.push({"id": id, "name": name})
+//     //             }
+//     //             this.storage.set('Saved stops', savedStops);
+//     //             this.loadBookmarkedStops();
+//     //     })
+//     // })
+// }
     removeBookmarkedStop(stop) {
         this.storage.get('Saved stops').then((savedStops) => {
             savedStops.splice(savedStops.indexOf(stop), 1);
